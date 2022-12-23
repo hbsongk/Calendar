d91c4a8f4ab67f37a6b4c64e6ffc34d0553a0e3c
hbsongk <68498323+hbsongk@users.noreply.github.com>
2022-12-23 12:01:07 +0900

    feat(fe): build calendar

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,24 @@
 <template>
   <div id="app">
-    <step-one />
+    <!-- <step-one />
     <step-two />
-    <step-three />
+    <step-three /> -->
+    <process-calendar />
   </div>
 </template>
 
 <script>
-import StepOne from "./components/StepOne.vue";
-import StepTwo from "./components/StepTwo.vue";
-import StepThree from "./components/StepThree.vue";
+// import StepOne from "./components/StepOne.vue";
+// import StepTwo from "./components/StepTwo.vue";
+// import StepThree from "./components/StepThree.vue";
+import ProcessCalendar from "./components/ProcessCalendar.vue";
 export default {
   name: "App",
   components: {
-    StepOne,
-    StepTwo,
-    StepThree,
+    // StepOne,
+    // StepTwo,
+    // StepThree,
+    ProcessCalendar,
   },
 };
 </script>
@@ -24,7 +27,7 @@ export default {
 #app {
   display: flex;
 }
-*{
-  font-weight:bold;
+* {
+  font-weight: bold;
 }
 </style>
---
+++ src/components/ProcessMachine.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+    <header>장비명</header>
+    <div v-for="(items, index) in machines" v-bind:key="index">
+      {{ items }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProcssMachine",
+  data() {
+    return {
+      machines: ["MCT_1", "MCT_2", "MCT_3", "CNC_1", "CNC_2", "CNC_3"],
+    };
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 8vw;
+  height: 8vh;
+  color: white;
+  background-color: red;
+  margin: 3px;
+  border: solid;
+  border-color: red;
+}
+.container > div {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 8vw;
+  height: 8vh;
+  color: white;
+  background-color: black;
+  margin: 6px 3px;
+  border: solid;
+  border-color: black;
+}
+</style>
